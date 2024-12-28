c6bc4bf5086e39fcf64ee35fd593d4a1742636c1
Adarsh Kumar <adarshff06@gmail.com>
2024-12-28 22:26:50 +0530

    first commit

---
+++ BACKEND/server.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import axios from 'axios';
+import cors from 'cors';
+
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({origin: '*'}));
+
+const API_KEY = process.env.API_KEY;
+const CHANNEL_ID = process.env.CHANNEL_ID;
+
+app.get('/live', async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${CHANNEL_ID}&eventType=live&type=video&key=${API_KEY}`
+    );
+
+    // console.log("data---",response.data.items);
+
+    const videos = response.data.items;
+
+    if (videos.length > 0) {
+      const liveVideo = videos[0];
+      res.send({
+        liveVideoTitle: liveVideo.snippet.title,
+        liveVideoURL: `https://www.youtube.com/watch?v=${liveVideo.id.videoId}`,
+      });
+    } else {
+      res.send({ message: 'No live stream is currently active.' });
+    }
+} catch (error) {
+    console.error('Error fetching live stream:', error.response ? error.response.data : error.message);
+    res.status(500).send({ error: 'Error fetching live stream data.', details: error.response?.data || error.message });
+  }
+  
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
